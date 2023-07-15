48bfef7bfe6f87cae15c7a4e4dedb8e2a77e4dbe
Asoreba-jnr <asorebajunior@yahoo.com>
2023-07-15 15:04:47 -0700

    Pusts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-/* main Entry point */
-int main (void)
+/**
+ * main - Entry point 
+ * 
+ * output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

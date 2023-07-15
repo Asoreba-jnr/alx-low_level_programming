40db5ca9d8c8b717680423601a6d58772fe994cc
Asoreba-jnr <asorebajunior@yahoo.com>
2023-07-15 15:54:43 -0700

    Pusts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- *main - Entry point 
- * 
- *output
- *
- *Return: Always 0 (Success)
+ * main - Entry point
+ * output
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
